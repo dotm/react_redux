c2ebc8aae713c26b2ce3df8f90ea2457c4e65d12
Yoshua Elmaryono <desde.el.mar96@gmail.com>
2018-05-18 21:06:11 +0700

    include redux from index

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import allReducer from './reducers'
+
+import './index.css';
+import App from './components/App';
+
+const store = createStore(allReducer)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
 registerServiceWorker();
