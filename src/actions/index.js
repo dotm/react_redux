2c77109e756f37b7c56dd3ee65e9c86cb4b4dfa0
Yoshua Elmaryono <desde.el.mar96@gmail.com>
2018-05-18 21:04:03 +0700

    add action creators

---
+++ src/actions/index.js
@@ -0,0 +1,29 @@
+import { generateRandomId } from 'util';
+
+/* action types */
+const ADD_USER = 'ADD_USER'
+const EDIT_USER = 'EDIT_USER'
+
+/* other constants */
+const UserList_SortingFilters = {
+    FIRST_NAME: 'FIRST_NAME',
+    LAST_NAME: 'LAST_NAME',
+    EMAIL: 'EMAIL',
+}
+const UserList_SortingOrder = {
+    ASCENDING: 'ASCENDING',
+    DESCENDING: 'DESCENDING',
+}
+
+/* action creators */
+function addUser({first_name, last_name, email}) {
+    let user_data = {
+        id: generateRandomId(),
+        first_name, last_name, email
+    }
+    return { type: ADD_USER, user_data }
+}
+function editUser({id, first_name, last_name, email}) {
+    let user_data = {id, first_name, last_name, email}
+    return { type: EDIT_USER, user_data }
+}
